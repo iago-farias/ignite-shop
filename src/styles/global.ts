cb41f9f8807a06f975bb9e66642ddc4e98c64677
iago-farias <iagox38@gmail.com>
2024-04-25 18:55:49 -0300

    feat: Implement home page layout.

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  "*": {
+    padding: 0,
+    margin: 0
+  },
+
+  body: {
+    '-webkit-font-smoothing': 'antialiased',
+    backgroundColor: "$gray900",
+    color: "$gray100"
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: "Roboto",
+    fontWeight: 400
+  }
+})
