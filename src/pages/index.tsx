78cade2c759aaf1d35ddd089fbe03f68be890a4d
iago-farias <iagox38@gmail.com>
2024-05-11 22:46:56 -0300

    feat: Implement checkout and success pages.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,20 @@ import Image from "next/image";
 import { GetStaticProps } from "next";
 import Stripe from "stripe";
 import { useKeenSlider } from 'keen-slider/react';
+import Link from "next/link";
 
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import "keen-slider/keen-slider.min.css";
 import { stripe } from "../lib/stripe";
+import Head from "next/head";
 
 interface HomeProps {
   products: {
     id: string;
     name: string;
     imageUrl: string;
-    price: number
+    price: string
   }[]
 }
 
@@ -26,20 +28,31 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {
-        products.map(product => (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {
+          products.map(product => (
+            <Link
+              key={product.id}
+              href={`/product/${product.id}`}
+              prefetch={false}
+            >
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        ))
-      }
-    </HomeContainer>
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          ))
+        }
+      </HomeContainer>
+    </>
   )
 }
 
